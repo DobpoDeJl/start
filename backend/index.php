14acf144177f29b203bb1933fc4a2020f5a3c705
Janis <yanik.dotki@mail.ru>
2022-01-03 00:59:45 +0300

    Added docker

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,31 @@
 <?php
 
-echo "Здравствуй мир";
+$arr = ['Mark', 'Den','Olga'];
+?>
+
+<ul>
+    <?php foreach($arr as $name): ?>
+    <li><?= $name ?></li>
+    <?php endforeach; ?>
+</ul>
+
+
+<div>
+    <h1>Текст</h1>
+    <h2></h2>
+    <h3></h3>
+    <p>Niuhgk huih iuhoiuhb</p>
+    <p>Подробнее прочитайте здесь - <a href="https://vk.com"> ссылка на vk</a></p>
+</div>
+<div>
+    Сколько мне лет?
+    <ul>
+        <li>12</li>
+        <li>20</li>
+        <li>30</li>
+        <li>больше 50</li>
+    </ul>
+</div>
+
+
+<img src="https://pic.rutubelist.ru/video/74/3c/743c697c23249aa00a5f4395444baec6.jpg" width="50%">
